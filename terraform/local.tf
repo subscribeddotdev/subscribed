cbf6eadb65bea39a70c08e438c856fb8fcf3a634
Firmino Changani <firmino.changani@gmail.com>
2024-06-10 20:04:05 +0100

    Create an ecr repo via terraform (#10)
    * add infra to setup the ECR repo
    
    * exclude tf state files
    
    * fmt

---
+++ terraform/local.tf
@@ -0,0 +1,6 @@
+locals {
+  common_tags = {
+    service_name = "subscribed-backend"
+    managed_by = "terraform"
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.53.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "subscribed-backend-prd"
+    key    = "state"
+    region = "eu-west-1"
+  }
+}

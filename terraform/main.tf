9815240032cafa67de58f428b4c77375236fac2e
Firmino Changani <firmino.changani@gmail.com>
2024-06-11 22:02:21 +0100

    import the db config to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,16 @@ resource "aws_ecr_repository" "subscribed_backend_prd" {
     scan_on_push = true
   }
 }
+
+resource "aws_db_instance" "subscribed_backend_postgres_prd" {
+  instance_class = "db.t3.micro"
+  storage_encrypted = true
+  tags = local.common_tags
+  performance_insights_enabled = true
+  skip_final_snapshot = true
+  copy_tags_to_snapshot = true
+  apply_immediately = false
+  max_allocated_storage = 1000
+}
+
+# TODO: Import the vpc config and the security group

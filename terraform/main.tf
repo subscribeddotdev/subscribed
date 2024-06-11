55cd74ce7380bfeb0d2841f1c9ba84b7361617eb
Firmino Changani <firmino.changani@gmail.com>
2024-06-11 22:02:43 +0100

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "subscribed_backend_prd" {
   name                 = "subscribed-backend-prd"
   image_tag_mutability = "IMMUTABLE"
-  tags = local.common_tags
+  tags                 = local.common_tags
 
   image_scanning_configuration {
     scan_on_push = true
@@ -9,14 +9,14 @@ resource "aws_ecr_repository" "subscribed_backend_prd" {
 }
 
 resource "aws_db_instance" "subscribed_backend_postgres_prd" {
-  instance_class = "db.t3.micro"
-  storage_encrypted = true
-  tags = local.common_tags
+  instance_class               = "db.t3.micro"
+  storage_encrypted            = true
+  tags                         = local.common_tags
   performance_insights_enabled = true
-  skip_final_snapshot = true
-  copy_tags_to_snapshot = true
-  apply_immediately = false
-  max_allocated_storage = 1000
+  skip_final_snapshot          = true
+  copy_tags_to_snapshot        = true
+  apply_immediately            = false
+  max_allocated_storage        = 1000
 }
 
 # TODO: Import the vpc config and the security group

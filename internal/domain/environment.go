ea9f2d3da9e6e1865ce8aec2af3ea7a5f9526200
Firmino Changani <firmino.changani@gmail.com>
2024-06-16 17:21:40 +0100

    refactor the environment entity and add tests

--- internal/domain/environment.go
+++ internal/domain/environment.go
@@ -1,18 +1,44 @@
 package domain
 
 import (
+	"strings"
 	"time"
+
+	"errors"
 )
 
 type Environment struct {
-	id           ID
-	name         string
-	orgID        ID
-	createdAt    time.Time
-	archivedAt   *time.Time
-	apiKeys      []ApiKey
-	applications []Application
-	eventTypes   []EventType
+	id         ID
+	orgID      ID
+	name       string
+	envType    EnvType
+	createdAt  time.Time
+	archivedAt *time.Time
+}
+
+func NewEnvironment(name string, orgID ID, envType EnvType) (*Environment, error) {
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return nil, errors.New("name cannot be empty")
+	}
+
+	err := envType.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	if orgID.IsEmpty() {
+		return nil, errors.New("orgID cannot be empty")
+	}
+
+	return &Environment{
+		id:         NewID(),
+		orgID:      orgID,
+		name:       name,
+		envType:    envType,
+		createdAt:  time.Now(),
+		archivedAt: nil,
+	}, nil
 }
 
 func (e *Environment) Id() ID {
@@ -35,14 +61,40 @@ func (e *Environment) ArchivedAt() *time.Time {
 	return e.archivedAt
 }
 
-func (e *Environment) ApiKeys() []ApiKey {
-	return e.apiKeys
+func (e *Environment) Type() EnvType {
+	return e.envType
 }
 
-func (e *Environment) Applications() []Application {
-	return e.applications
+type EnvType struct {
+	name string
 }
 
-func (e *Environment) EventTypes() []EventType {
-	return e.eventTypes
+func (e EnvType) String() string {
+	return e.name
+}
+
+var (
+	EnvTypeProduction  = EnvType{"production"}
+	EnvTypeDevelopment = EnvType{"development"}
+)
+
+func (e EnvType) Validate() error {
+	_, err := marshallToEventType(e.name)
+	return err
+}
+
+func marshallToEventType(name string) (EnvType, error) {
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return EnvType{}, errors.New("environment type cannot be empty")
+	}
+
+	switch name {
+	case EnvTypeProduction.name:
+		return EnvTypeProduction, nil
+	case EnvTypeDevelopment.name:
+		return EnvTypeDevelopment, nil
+	default:
+		return EnvType{}, errors.New("invalid environment type")
+	}
 }
---
+++ internal/domain/environment_test.go
@@ -0,0 +1,69 @@
+package domain_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/subscribeddotdev/subscribed-backend/internal/domain"
+)
+
+func TestNewEnvironment(t *testing.T) {
+	testCases := []struct {
+		name        string
+		expectedErr string
+
+		envName string
+		envType domain.EnvType
+		orgID   domain.ID
+	}{
+		{
+			name:        "create_new_environment",
+			expectedErr: "",
+			envName:     "Development",
+			orgID:       domain.NewID(),
+			envType:     domain.EnvTypeDevelopment,
+		},
+		{
+			name:        "error_empty_name",
+			expectedErr: "name cannot be empty",
+			envName:     "  ",
+			orgID:       domain.NewID(),
+			envType:     domain.EnvTypeDevelopment,
+		},
+		{
+			name:        "error_empty_or_invalid_org_id",
+			expectedErr: "orgID cannot be empty",
+			envName:     "Production",
+			orgID:       domain.ID{},
+			envType:     domain.EnvTypeProduction,
+		},
+		{
+			name:        "error_empty_env_type",
+			expectedErr: "environment type cannot be empty",
+			envName:     "Production",
+			orgID:       domain.NewID(),
+			envType:     domain.EnvType{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			env, err := domain.NewEnvironment(tc.envName, tc.orgID, tc.envType)
+
+			if tc.expectedErr != "" {
+				assert.EqualError(t, err, tc.expectedErr)
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, tc.envName, env.Name())
+			assert.Equal(t, tc.orgID, env.OrgID())
+			assert.Equal(t, tc.envType, env.Type())
+			assert.True(t, env.CreatedAt().Before(time.Now()))
+		})
+	}
+}

22e9df66889d1b750a7ebc404f1dae014e363189
Firmino Changani <firmino.changani@gmail.com>
2024-06-19 16:40:51 +0100

    implement the endpoint

--- internal/ports/http/handlers.go
+++ internal/ports/http/handlers.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/subscribeddotdev/subscribed-backend/internal/app"
+	"github.com/subscribeddotdev/subscribed-backend/internal/app/command"
+	"github.com/subscribeddotdev/subscribed-backend/internal/domain"
 )
 
 type handlers struct {
@@ -12,9 +14,34 @@ type handlers struct {
 	loginProviderWebhookVerifier LoginProviderWebhookVerifier
 }
 
-func (h handlers) AddEndpoint(ctx echo.Context, applicationID string) error {
-	//TODO implement me
-	panic("implement me")
+func (h handlers) AddEndpoint(c echo.Context, applicationID string) error {
+	appID, err := domain.NewIdFromString(applicationID)
+	if err != nil {
+		return NewHandlerErrorWithStatus(err, "invalid-application-id", http.StatusBadRequest)
+	}
+
+	var body AddEndpointRequest
+	err = c.Bind(&body)
+	if err != nil {
+		return NewHandlerErrorWithStatus(err, "error-parsing-the-body", http.StatusBadRequest)
+	}
+
+	var description string
+	if body.Description != nil {
+		description = *body.Description
+	}
+
+	err = h.application.Command.AddEndpoint.Execute(c.Request().Context(), command.AddEndpoint{
+		ApplicationID:          appID,
+		EndpointUrl:            body.Url,
+		Description:            description,
+		EventTypesSubscribedTo: nil,
+	})
+	if err != nil {
+		return NewHandlerError(err, "unable-to-add-endpoint")
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
 
 type LoginProviderWebhookVerifier interface {

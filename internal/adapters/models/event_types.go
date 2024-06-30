2deafbd94c7a4f075dad7f2ee75cb813ffcf13aa
Firmino Changani <firmino.changani@gmail.com>
2024-06-30 12:35:10 +0100

    refactor: add missing constraints to the message relation (#29)

--- internal/adapters/models/event_types.go
+++ internal/adapters/models/event_types.go
@@ -101,14 +101,17 @@ var EventTypeWhere = struct {
 
 // EventTypeRels is where relationship names are stored.
 var EventTypeRels = struct {
-	Org string
+	Org      string
+	Messages string
 }{
-	Org: "Org",
+	Org:      "Org",
+	Messages: "Messages",
 }
 
 // eventTypeR is where relationships are stored.
 type eventTypeR struct {
-	Org *Organization `boil:"Org" json:"Org" toml:"Org" yaml:"Org"`
+	Org      *Organization `boil:"Org" json:"Org" toml:"Org" yaml:"Org"`
+	Messages MessageSlice  `boil:"Messages" json:"Messages" toml:"Messages" yaml:"Messages"`
 }
 
 // NewStruct creates a new relationship struct
@@ -121,6 +124,13 @@ func (r *eventTypeR) GetOrg() *Organization {
 		return nil
 	}
 	return r.Org
+}
+
+func (r *eventTypeR) GetMessages() MessageSlice {
+	if r == nil {
+		return nil
+	}
+	return r.Messages
 }
 
 // eventTypeL is where Load methods for each relationship are stored.
@@ -423,6 +433,20 @@ func (o *EventType) Org(mods ...qm.QueryMod) organizationQuery {
 	return Organizations(queryMods...)
 }
 
+// Messages retrieves all the message's Messages with an executor.
+func (o *EventType) Messages(mods ...qm.QueryMod) messageQuery {
+	var queryMods []qm.QueryMod
+	if len(mods) != 0 {
+		queryMods = append(queryMods, mods...)
+	}
+
+	queryMods = append(queryMods,
+		qm.Where("\"messages\".\"event_type_id\"=?", o.ID),
+	)
+
+	return Messages(queryMods...)
+}
+
 // LoadOrg allows an eager lookup of values, cached into the
 // loaded structs of the objects. This is for an N-1 relationship.
 func (eventTypeL) LoadOrg(ctx context.Context, e boil.ContextExecutor, singular bool, maybeEventType interface{}, mods queries.Applicator) error {
@@ -543,6 +567,120 @@ func (eventTypeL) LoadOrg(ctx context.Context, e boil.ContextExecutor, singular 
 	return nil
 }
 
+// LoadMessages allows an eager lookup of values, cached into the
+// loaded structs of the objects. This is for a 1-M or N-M relationship.
+func (eventTypeL) LoadMessages(ctx context.Context, e boil.ContextExecutor, singular bool, maybeEventType interface{}, mods queries.Applicator) error {
+	var slice []*EventType
+	var object *EventType
+
+	if singular {
+		var ok bool
+		object, ok = maybeEventType.(*EventType)
+		if !ok {
+			object = new(EventType)
+			ok = queries.SetFromEmbeddedStruct(&object, &maybeEventType)
+			if !ok {
+				return errors.New(fmt.Sprintf("failed to set %T from embedded struct %T", object, maybeEventType))
+			}
+		}
+	} else {
+		s, ok := maybeEventType.(*[]*EventType)
+		if ok {
+			slice = *s
+		} else {
+			ok = queries.SetFromEmbeddedStruct(&slice, maybeEventType)
+			if !ok {
+				return errors.New(fmt.Sprintf("failed to set %T from embedded struct %T", slice, maybeEventType))
+			}
+		}
+	}
+
+	args := make([]interface{}, 0, 1)
+	if singular {
+		if object.R == nil {
+			object.R = &eventTypeR{}
+		}
+		args = append(args, object.ID)
+	} else {
+	Outer:
+		for _, obj := range slice {
+			if obj.R == nil {
+				obj.R = &eventTypeR{}
+			}
+
+			for _, a := range args {
+				if a == obj.ID {
+					continue Outer
+				}
+			}
+
+			args = append(args, obj.ID)
+		}
+	}
+
+	if len(args) == 0 {
+		return nil
+	}
+
+	query := NewQuery(
+		qm.From(`messages`),
+		qm.WhereIn(`messages.event_type_id in ?`, args...),
+	)
+	if mods != nil {
+		mods.Apply(query)
+	}
+
+	results, err := query.QueryContext(ctx, e)
+	if err != nil {
+		return errors.Wrap(err, "failed to eager load messages")
+	}
+
+	var resultSlice []*Message
+	if err = queries.Bind(results, &resultSlice); err != nil {
+		return errors.Wrap(err, "failed to bind eager loaded slice messages")
+	}
+
+	if err = results.Close(); err != nil {
+		return errors.Wrap(err, "failed to close results in eager load on messages")
+	}
+	if err = results.Err(); err != nil {
+		return errors.Wrap(err, "error occurred during iteration of eager loaded relations for messages")
+	}
+
+	if len(messageAfterSelectHooks) != 0 {
+		for _, obj := range resultSlice {
+			if err := obj.doAfterSelectHooks(ctx, e); err != nil {
+				return err
+			}
+		}
+	}
+	if singular {
+		object.R.Messages = resultSlice
+		for _, foreign := range resultSlice {
+			if foreign.R == nil {
+				foreign.R = &messageR{}
+			}
+			foreign.R.EventType = object
+		}
+		return nil
+	}
+
+	for _, foreign := range resultSlice {
+		for _, local := range slice {
+			if local.ID == foreign.EventTypeID {
+				local.R.Messages = append(local.R.Messages, foreign)
+				if foreign.R == nil {
+					foreign.R = &messageR{}
+				}
+				foreign.R.EventType = local
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
 // SetOrg of the eventType to the related item.
 // Sets o.R.Org to related.
 // Adds o to related.R.OrgEventTypes.
@@ -587,6 +725,59 @@ func (o *EventType) SetOrg(ctx context.Context, exec boil.ContextExecutor, inser
 		related.R.OrgEventTypes = append(related.R.OrgEventTypes, o)
 	}
 
+	return nil
+}
+
+// AddMessages adds the given related objects to the existing relationships
+// of the event_type, optionally inserting them as new records.
+// Appends related to o.R.Messages.
+// Sets related.R.EventType appropriately.
+func (o *EventType) AddMessages(ctx context.Context, exec boil.ContextExecutor, insert bool, related ...*Message) error {
+	var err error
+	for _, rel := range related {
+		if insert {
+			rel.EventTypeID = o.ID
+			if err = rel.Insert(ctx, exec, boil.Infer()); err != nil {
+				return errors.Wrap(err, "failed to insert into foreign table")
+			}
+		} else {
+			updateQuery := fmt.Sprintf(
+				"UPDATE \"messages\" SET %s WHERE %s",
+				strmangle.SetParamNames("\"", "\"", 1, []string{"event_type_id"}),
+				strmangle.WhereClause("\"", "\"", 2, messagePrimaryKeyColumns),
+			)
+			values := []interface{}{o.ID, rel.ID}
+
+			if boil.IsDebug(ctx) {
+				writer := boil.DebugWriterFrom(ctx)
+				fmt.Fprintln(writer, updateQuery)
+				fmt.Fprintln(writer, values)
+			}
+			if _, err = exec.ExecContext(ctx, updateQuery, values...); err != nil {
+				return errors.Wrap(err, "failed to update foreign table")
+			}
+
+			rel.EventTypeID = o.ID
+		}
+	}
+
+	if o.R == nil {
+		o.R = &eventTypeR{
+			Messages: related,
+		}
+	} else {
+		o.R.Messages = append(o.R.Messages, related...)
+	}
+
+	for _, rel := range related {
+		if rel.R == nil {
+			rel.R = &messageR{
+				EventType: o,
+			}
+		} else {
+			rel.R.EventType = o
+		}
+	}
 	return nil
 }
 
---
+++ tests/fixture/event_type.go
@@ -0,0 +1,67 @@
+package fixture
+
+import (
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/subscribeddotdev/subscribed-backend/internal/adapters/models"
+	"github.com/subscribeddotdev/subscribed-backend/internal/domain"
+	"github.com/subscribeddotdev/subscribed-backend/tests"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+type EventType struct {
+	factory *Factory
+	model   models.EventType
+}
+
+func (a *EventType) WithOrgID(value string) *EventType {
+	a.model.OrgID = value
+	return a
+}
+
+func (a *EventType) WithName(value string) *EventType {
+	a.model.Name = value
+	return a
+}
+
+func (a *EventType) WithDescription(value string) *EventType {
+	a.model.Description = null.StringFrom(value)
+	return a
+}
+
+func (a *EventType) WithSchema(value string) *EventType {
+	a.model.Schema = null.StringFrom(value)
+	return a
+}
+
+func (a *EventType) WithSchemaExample(value string) *EventType {
+	a.model.SchemaExample = null.StringFrom(value)
+	return a
+}
+
+func (a *EventType) WithArchivedAt(value time.Time) *EventType {
+	a.model.ArchivedAt = null.TimeFrom(value)
+	return a
+}
+
+func (a *EventType) Save() models.EventType {
+	err := a.model.Insert(a.factory.ctx, a.factory.db, boil.Infer())
+	require.NoError(a.factory.t, err)
+
+	return a.model
+}
+
+func (a *EventType) NewDomainModel() *domain.EventType {
+	return domain.UnMarshallEventType(
+		tests.MustID(a.factory.t, a.model.ID),
+		tests.MustID(a.factory.t, a.model.OrgID),
+		a.model.Name,
+		a.model.Description.String,
+		a.model.Schema.String,
+		a.model.SchemaExample.String,
+		a.model.CreatedAt,
+		a.model.ArchivedAt.Ptr(),
+	)
+}

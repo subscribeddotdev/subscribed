ec85b77ea073b63f67c107f4c18e0e46b4e25ccb
Firmino Changani <firmino.changani@gmail.com>
2024-08-30 21:49:23 +0100

    make sure the pagination parameters are always set

--- internal/app/query/models.go
+++ internal/app/query/models.go
@@ -1,14 +1,39 @@
 package query
 
 type PaginationParams struct {
-	Page  *int
-	Limit *int
+	page  int
+	limit int
+}
+
+func NewPaginationParams(page, limit *int) PaginationParams {
+	p := 1
+	l := 15
+	if page != nil {
+		p = *page
+	}
+
+	if limit != nil {
+		l = *limit
+	}
+
+	return PaginationParams{
+		page:  p,
+		limit: l,
+	}
+}
+
+func (p *PaginationParams) Page() int {
+	return p.page
+}
+
+func (p *PaginationParams) Limit() int {
+	return p.limit
 }
 
 type Paginated[D any] struct {
-	Total       int64
-	PerPage     int64
-	CurrentPage int64
-	TotalPages  int64
+	Total       int
+	PerPage     int
+	CurrentPage int
+	TotalPages  int
 	Data        D
 }

600a9b9f9a315b3519940beee5052c864217b7b0
Firmino Changani <firmino.changani@gmail.com>
2024-09-08 08:53:08 +0100

    add component test

--- server/tests/components/event_types_test.go
+++ server/tests/components/event_types_test.go
@@ -1,12 +1,14 @@
 package components_test
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/subscribeddotdev/subscribed/server/internal/adapters/models"
 	"github.com/subscribeddotdev/subscribed/server/tests/client"
 	"github.com/subscribeddotdev/subscribed/server/tests/fixture"
 )
@@ -18,10 +20,40 @@ func TestEventTypes(t *testing.T) {
 	token := signIn(t, member.Email, password)
 	apiClient := getClientWithToken(t, token)
 
-	resp, err := apiClient.CreateEventType(ctx, client.CreateEventTypeRequest{
-		Name:        gofakeit.AppName(),
-		Description: toPtr(gofakeit.Sentence(20)),
+	t.Run("create_event_type", func(t *testing.T) {
+		resp, err := apiClient.CreateEventType(ctx, client.CreateEventTypeRequest{
+			Name:        fmt.Sprintf("%s.%s", gofakeit.Noun(), gofakeit.Verb()),
+			Description: toPtr(gofakeit.Sentence(20)),
+		})
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusCreated, resp.StatusCode)
 	})
-	require.NoError(t, err)
-	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	t.Run("get_all_event_types_by_org_id", func(t *testing.T) {
+		eventTypesFixtureCount := 20
+		eventTypesFixture := make([]models.EventType, eventTypesFixtureCount)
+
+		for i := 0; i < eventTypesFixtureCount; i++ {
+			eventTypesFixture[i] = ff.NewEventType().WithOrgID(org.ID).Save()
+		}
+
+		perPage := 5
+		totalPages := eventTypesFixtureCount / 5
+
+		for i := 0; i < totalPages; i++ {
+			currentPage := i + 1
+			resp, err := apiClient.GetEventTypesWithResponse(ctx, &client.GetEventTypesParams{
+				Limit: &perPage,
+				Page:  &currentPage,
+			})
+
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, resp.StatusCode())
+			require.NotEmpty(t, resp.JSON200.Data)
+			require.Equal(t, currentPage, resp.JSON200.Pagination.CurrentPage)
+			require.GreaterOrEqual(t, resp.JSON200.Pagination.Total, eventTypesFixtureCount)
+			require.Equal(t, perPage, resp.JSON200.Pagination.PerPage)
+			require.Equal(t, totalPages, resp.JSON200.Pagination.TotalPages)
+		}
+	})
 }

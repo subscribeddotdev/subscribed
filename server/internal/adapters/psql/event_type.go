edda570c2a7769b8c124538c39245f7931a01203
Firmino Changani <firmino.changani@gmail.com>
2024-09-08 08:00:45 +0100

    wip

--- server/internal/adapters/psql/event_type.go
+++ server/internal/adapters/psql/event_type.go
@@ -10,6 +10,7 @@ import (
 	"github.com/subscribeddotdev/subscribed/server/internal/domain/iam"
 	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 type EventTypeRepository struct {
@@ -47,5 +48,37 @@ func (e EventTypeRepository) FindAll(
 	orgID iam.OrgID,
 	pagination query.PaginationParams,
 ) (query.Paginated[[]domain.EventType], error) {
+	total, err := models.Applications(
+		//TODO: include orgID in the query
+		models.ApplicationWhere.EnvironmentID.EQ(envID.String()),
+	).Count(ctx, e.db)
+	if err != nil {
+		return query.Paginated[[]domain.EventType]{}, fmt.Errorf("error counting applications: %v", err)
+	}
+
+	rows, err := models.EventTypes(
+		//TODO: include orgID in the query
+		// models.EventTypeWhere.OrgID.EQ(orgID),
+		models.ApplicationWhere.EnvironmentID.EQ(envID.String()),
+		qm.Offset(mapPaginationParamsToSqlOffset(pagination)),
+		qm.Limit(pagination.Limit()),
+		qm.OrderBy("created_at DESC"),
+	).All(ctx, e.db)
+	if err != nil {
+		return query.Paginated[[]domain.EventType]{}, fmt.Errorf("error querying applications: %v", err)
+	}
+
+	return query.Paginated[[]domain.EventType]{
+		Total:       int(total),
+		PerPage:     len(rows),
+		CurrentPage: pagination.Page(),
+		TotalPages:  getPaginationTotalPages(total, pagination.Limit()),
+		Data:        mapRowsToEventTypes(rows),
+	}, nil
+
 	return query.Paginated[[]domain.EventType]{}, nil
+}
+
+func mapRowsToEventTypes(rows []*models.EventType) []domain.EventType {
+	return nil
 }

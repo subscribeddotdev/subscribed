c74dd1e2e63f6b2716106fc56db55191b3924d7d
Firmino Changani <firmino.changani@gmail.com>
2024-09-10 17:52:37 +0100

    add breadcrumbs (#79)

--- web/src/common/layouts/LayoutDashboard/LayoutDashboard.tsx
+++ web/src/common/layouts/LayoutDashboard/LayoutDashboard.tsx
@@ -1,3 +1,4 @@
+import { Breadcrumbs } from "@@/common/components/Breadcrumbs/Breadcrumbs";
 import { EnvironmentSelector } from "@@/modules/EnvironmentSelector/EnvironmentSelector";
 import { UserMenu } from "@@/modules/UserMenu/UserMenu";
 import { Heading } from "@radix-ui/themes";
@@ -5,9 +6,13 @@ import { PropsWithChildren } from "react";
 import styles from "./LayoutDashboard.module.css";
 import { MenuList } from "./Menu";
 
-interface Props extends PropsWithChildren {}
+interface Props extends PropsWithChildren {
+  breadcrumbs?: {
+    variables: Record<string, string>;
+  };
+}
 
-export function LayoutDashboard({ children }: Props) {
+export function LayoutDashboard({ children, breadcrumbs }: Props) {
   return (
     <>
       <aside className={styles.sidebar}>
@@ -25,8 +30,10 @@ export function LayoutDashboard({ children }: Props) {
           <div></div>
           <UserMenu />
         </header>
-
-        <div className={styles.content}>{children}</div>
+        <div className={styles.content}>
+          {!!breadcrumbs && <Breadcrumbs renderFromIndex={2} variables={breadcrumbs.variables} />}
+          {children}
+        </div>
       </main>
     </>
   );
